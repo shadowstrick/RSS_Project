6ea87410393195d10b278a28beb29ec19d447352
shadowstrick <alix.fauqueur@epitech.eu>
2014-09-13 16:11:43 +0200

    debug christ

--- RSS_PC/src/main.java
+++ RSS_PC/src/main.java
@@ -3,8 +3,7 @@ public class main {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-    System.print("Alix sait faire des pushs maintenant !");
-
+		System.out.print("Alix sait faire des pushs maintenant !");
 	}
 
 }
